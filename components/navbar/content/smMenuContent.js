7b3d5f2be996ade9395686d102e213bcb3732812
KostuX <77516813+KostuX@users.noreply.github.com>
2024-11-07 00:41:21 +0000

    user profile in small menu

--- components/navbar/content/smMenuContent.js
+++ components/navbar/content/smMenuContent.js
@@ -1,17 +1,18 @@
 import { NavbarMenu } from "@nextui-org/react";
 import { MenuItems } from "@/components/navbar/content/menuItems";
 import { OAuth } from "@/components/navbar/content/OAuth/OAuth";
-import { User } from "@/components/navbar/content/user/user";
+import { UserContent } from "@/components/navbar/content/user/userContent";
 export const SmMenuContent = (props) => {
-  let loggedIn = props?.user_session;
+  let user_session = props?.user_session;
+
   return (
     <NavbarMenu>
       <div className=" flex flex-col gap-4  text-center   place-content-center">
         <MenuItems />
       </div>
       <div className=" mt-10  text-center px-10 place-content-center">
-        {loggedIn ? (
-          <User />
+        {user_session ? (
+          <UserContent user={user_session} />
         ) : (
           <div>
             <OAuth />
