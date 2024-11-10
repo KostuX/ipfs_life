729b31d2cb263246b25e6b28512a7acc15ff1210
KostuX <77516813+KostuX@users.noreply.github.com>
2024-11-10 01:56:05 +0000

    todo: manage cross-component useeffect

--- components/index/inputComponent.js
+++ components/index/inputComponent.js
@@ -1,46 +1,19 @@
-import {
-  Button,
-  Input,
-  Image,
-  ButtonGroup,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-  Spinner,
-  Select,
-  SelectItem,
-  Progress,
-} from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
 
-import Grid from "@mui/material/Grid";
-import React, { useState, useEffect } from "react";
-export const InputComponent = () => {
-  let searchCID = "";
-  const [input, setInput] = useState(searchCID);
- 
+import myValidator from "@/pages/api/classes/myValidator";
+export const InputComponent = (input, onInputChange) => {
+  const validator = new myValidator();
+  // TODO: add validator
 
-  function cidInput() {
-    return (
-      <Input
-        type="text"
-        placeholder={input ? input : "Please Enter Content-ID"}
-        onInput={(e) => setInput(validator.clean(e.target.value))}
-      />
-    );
-  }
-    return    <Grid
-    gap={2}
-    container
-    wrap="nowrap"
-    item={true}
-    justify="center"
-    style={{ marginTop: 50 }}
-  >
-    <Grid xs={2} item={true}></Grid>
-    <Grid xs={8} item={true} justify="center">
-      {cidInput()}
-    </Grid>
-    <Grid xs={2} item={true}></Grid>
-  </Grid>
-}
+  console.log(onInputChange);
+
+  input = input.input;
+
+  return (
+    <Input
+      type="text"
+      placeholder={input ? input : "Please Enter Content-ID"}
+      onInput={(e) => onInputChange(validator.clean(e.target.value))}
+    />
+  );
+};
